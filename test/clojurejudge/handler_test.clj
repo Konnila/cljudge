382e023f76ec8e12b6b8936c2bece3f14d45b223
Toni Könnilä <toni.konnila@siili.com>
2018-10-28 18:04:48 +0200

    'fixed test'

--- test/clojurejudge/handler_test.clj
+++ test/clojurejudge/handler_test.clj
@@ -4,11 +4,6 @@
             [clojurejudge.handler :refer :all]))
 
 (deftest test-app
-  (testing "main route"
-    (let [response (app (mock/request :get "/"))]
-      (is (= (:status response) 200))
-      (is (= (:body response) "Hello World and also Hello Tuomo! Tinkering with Circle CI and clojure project autodeploy :-)"))))
-
   (testing "not-found route"
     (let [response (app (mock/request :get "/invalid"))]
       (is (= (:status response) 404)))))
